06207e1a068a8879d308069c39efccf5a0d38cd2
PigSnowball <kvilliers@gmail.com>
2012-02-17 21:53:00 +1100

    Working on my xoom, time to get a Google Places Search working... hrmm fragments....

---
+++ src/com/porknbunny/onmyway/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.porknbunny.onmyway;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pigsnowball
+ * Date: 17/02/2012
+ * Time: 21:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class SearchActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
