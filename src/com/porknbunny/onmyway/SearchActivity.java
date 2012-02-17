afcec360f712cb77433e26d87e51ef80d8672a50
PigSnowball <kvilliers@gmail.com>
2012-02-17 23:45:39 +1100

    search actionbar item is up

--- src/com/porknbunny/onmyway/SearchActivity.java
+++ src/com/porknbunny/onmyway/SearchActivity.java
@@ -2,6 +2,8 @@ package com.porknbunny.onmyway;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.SearchView;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,9 +13,20 @@ import android.os.Bundle;
  * To change this template use File | Settings | File Templates.
  */
 public class SearchActivity extends Activity {
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.search_activity);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        // Configure the search info and add any event listeners
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
